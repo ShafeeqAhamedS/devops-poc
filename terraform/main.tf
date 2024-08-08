e16f2f3f92a161e7f348834dfb3a85d5112c6f88
ShafeeqAhamedS <shafeeqahamed2804@gmail.com>
2024-08-09 00:33:17 +0530

    Edited main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "local" {}
 resource "null_resource" "build_and_deploy" {
   provisioner "local-exec" {
     command = <<EOT
-    cd C:\Users\shafe\Downloads\Working Files\devops\react-app\ && npm install && npm run build && npx serve -s build
+    cd .. && npm install && npm run build && npx serve -s build
     EOT
   }
 }
